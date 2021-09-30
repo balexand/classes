094748b3dbab55a43afb75d7b769807d6677701b
Brian Alexander <balexand@gmail.com>
2021-09-30 10:00:23 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Classes.MixProject do
     [
       app: :classes,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
