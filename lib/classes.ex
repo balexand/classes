9a902f7edf445da637527c869a5e92bd2fb2146b
Brian Alexander <balexand@gmail.com>
2021-09-30 10:42:24 -0600

    implement

--- lib/classes.ex
+++ lib/classes.ex
@@ -1,18 +1,58 @@
 defmodule Classes do
   @moduledoc """
-  Documentation for `Classes`.
+  A pure function for generating CSS classes.
   """
 
   @doc """
-  Hello world.
+  Returns a string containing CSS. The argument can be any of the following:
+
+    * String or atom containing a CSS class or classes.
+    * Keyword list or map with string or atom keys. The key is a CSS class and the value is
+      treated as a boolean indicating whether or not the class should be included.
+    * `nil` and `false` are filtered.
+    * Lists, including deeply nested lists, containing any of the above types.
 
   ## Examples
 
-      iex> Classes.classes()
-      :world
+      iex> classes("my-class")
+      "my-class"
 
+      iex> classes(:myclass)
+      "myclass"
+
+      iex> classes(["one", :two])
+      "one two"
+
+      iex> classes(yes: true, no: false)
+      "yes"
+
+      iex> classes(%{"string-key" => true, "atom-key": true})
+
+      iex> classes(yes: "I'm truthy", no: nil)
+      "yes"
+
+      iex> classes([%{"one" => true}, [two: false, three: true]])
+      "one three"
+
+      iex> classes([{"string-key-tuple", true}, {"also-happens-to_work", nil}])
+      "string-key-tuple"
+
+      iex> classes([{"invalid argument"}])
+      ** (FunctionClauseError) no function clause matching in Classes.class_list/1
   """
-  def classes do
-    :world
+  def classes(class) do
+    [class]
+    |> List.flatten()
+    |> Enum.flat_map(&class_list/1)
+    |> Enum.join(" ")
   end
+
+  defguardp is_class(c) when is_binary(c) or is_atom(c)
+
+  # Returns list of strings and atoms
+  defp class_list(s) when is_class(s), do: [s]
+  defp class_list({k, false}) when is_class(k), do: []
+  defp class_list({k, nil}) when is_class(k), do: []
+  defp class_list({k, _}) when is_class(k), do: [k]
+  defp class_list(%{} = map), do: Enum.flat_map(map, &class_list/1)
 end
--- test/classes_test.exs
+++ test/classes_test.exs
@@ -1,8 +1,4 @@
 defmodule ClassesTest do
   use ExUnit.Case, async: true
-  doctest Classes
-
-  test "greets the world" do
-    assert Classes.classes() == :world
-  end
+  doctest Classes, import: true
 end
