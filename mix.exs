572e15484fcc174d85409af577ba31a2102535e8
Brian Alexander <balexand@gmail.com>
2021-09-30 10:09:48 -0600

    CI (#1)
    * Create elixir.yml
    
    * Update mix.exs
    
    * ci
    
    * Update mix.exs
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir.yml
    
    * Update elixir-pre-1.12.yml
    
    * Update elixir.yml
    
    * ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Classes.MixProject do
     [
       app: :classes,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
